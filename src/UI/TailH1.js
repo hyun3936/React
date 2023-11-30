04fa3d287e56e17fc60fdb00028f84d925b5c336
hyun3936 <hyun3936@naver.com>
2023-12-01 08:46:06 +0900

    11/30 수정본

--- src/UI/TailH1.js
+++ src/UI/TailH1.js
@@ -1,6 +1,6 @@
-export default function TailH1() {
+export default function TailH1({title}) {
   return (
-    <div className="text-4xl text-sky-900" font-sans font-bold >
+    <div className="text-4xl text-sky-900 font-sans font-bold">
       {title}
     </div>
   )
